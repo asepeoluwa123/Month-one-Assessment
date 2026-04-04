61b76002f22f0471b1f7ec15a0bb2a4975dc3951
Asepeoluwa123 <your-email@example.com>
2026-04-04 02:06:09 +0100

    TechCorp Terraform Assessment Submission

--- main.tf
+++ main.tf
@@ -122,7 +122,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -132,7 +132,7 @@ resource "aws_security_group" "bastion_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-resource "aws_security_group" "web_sg" {
+resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
@@ -149,12 +149,48 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "techcorp-alb-sg" }
+}
+
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
   ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "techcorp-web-sg" }
 }
 resource "aws_security_group" "db_sg" {
   vpc_id = aws_vpc.main.id
@@ -172,12 +208,22 @@ resource "aws_security_group" "db_sg" {
     protocol  = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "techcorp-db-sg" }
 }
 resource "aws_instance" "bastion" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public1.id
   key_name      = var.key_name
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
 
   tags = { Name = "Bastion Host" }
@@ -210,20 +256,24 @@ resource "aws_lb" "alb" {
     aws_subnet.public1.id,
     aws_subnet.public2.id
   ]
+  security_groups = [aws_security_group.alb_sg.id]
 }
 
 resource "aws_lb_target_group" "web" {
-  name     = "techcorp-web-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "techcorp-web-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
   health_check {
     path                = "/"
     protocol            = "HTTP"
     healthy_threshold   = 2
     unhealthy_threshold = 2
-    interval            = 30
-    timeout             = 5
+    interval            = 20
+    timeout             = 10
+    matcher             = "200-299"
   }
 }
 
